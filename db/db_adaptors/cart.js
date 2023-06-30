4ae9f9329ba075b3f77960674b2c6995143c604f
rickychong43 <ricky43@vt.edu>
2023-06-30 18:11:00 -0400

    cart changes 1

--- db/db_adaptors/cart.js
+++ db/db_adaptors/cart.js
@@ -10,11 +10,54 @@ async function createCart({ userId, isActive, favorites }) {
     `, [ userId, isActive, favorites ]);
     return cart;
   } catch (error) {
-    console.error("Error creating user!", error);
+    console.error("Error creating cart!", error);
     throw error;
   }
 }
 
+async function addCartToUser({
+  cartId,
+  userId,
+}) {
+  try {
+    const {
+      rows: [cart],
+    } = await client.query(
+      `
+      INSERT INTO routine_activities ("cartId, userId")
+      VALUES ($1, $2)
+      ON CONFLICT ("cartId, userId") DO NOTHING
+      RETURNING *;
+    `,
+      [cartId, userId]
+    );
+
+    return cart;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
 module.exports = {
   createCart,  
+  totalAmount
 };
+
+
+
+// async function totalAmount({ price }){
+//   try {
+//     const { rows: [ total ] } = await client.query(/*sql*/`
+//       SELECT *
+//       FROM cart
+//       WHERE id =$1
+//       ON CONFLICT (username) DO NOTHING
+//       RETURNING *;
+//     `, [ price ]);
+//     return total;
+//   } catch (error) {
+//     console.error("Error creating cart!", error);
+//     throw error;
+//   }
+// }
