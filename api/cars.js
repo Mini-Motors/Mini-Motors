f63d3baa952f2d7722e64a9328fa837a07aeb019
Kevin Harp <kharp1984@gmail.com>
2023-07-02 16:50:53 -0400

    updated and 99 percent working API

--- api/cars.js
+++ api/cars.js
@@ -9,7 +9,29 @@ const {
   getCarsByColor,
   getCarsById,
   updateCar,
+  getActiveCartsByCarId,
+  createCar
 } = require("../db/db_adaptors");
+
+
+// GET /api/cars/:carId/cart
+carsRouter.get('/:carId/cart', async (req, res, next) => {
+  const carId = req.params.carId;
+  try {
+    const activeCarts = await getActiveCartsByCarId({ id: carId });
+    if (activeCarts  && activeCarts.length) {
+      res.send(activeCarts);
+    } else {
+      throw ({
+        error: "Duplicates",
+        name: "Carts",
+        message: "Active cart not found"
+      })
+    }
+  } catch (error) {
+    next(error);
+  } 
+});
 
 // GET /cars
 carsRouter.get("/", async (req, res, next) => {
@@ -20,6 +42,84 @@ carsRouter.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+
+// POST /api/cars
+carsRouter.post("/", async (req, res, next) => {
+  const request = req.body;
+  try {
+    const existingActivity = await getCarsById(request.id);
+    if (!existingActivity) {
+      const { manufacturer, model, type, color, price } = await createCar(request);    
+      res.send({ manufacturer, model, type, color, price });
+    } else {
+      throw ({
+        error: "Duplicates",
+        name: "Activity",
+        message: "Car already exists!"
+      })
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PATCH /cars/:cardId
+carsRouter.patch("/:carId", async (req, res, next) => {
+  const { carId } = req.params;
+  const updateFields = {};
+  const { manufacturer, model, type, color, price } = req.body;
+
+  if (manufacturer) {
+    updateFields.manufacturer = manufacturer;
+  }
+
+  if (model) {
+    updateFields.model = model;
+  }
+
+  if (type) {
+    updateFields.type = type;
+  }
+
+  if (color) {
+    updateFields.color = color;
+  }
+
+  if (price) {
+    updateFields.price = price;
+  }
+
+  try {
+    const originalCar = await getCarsById(carId);
+    if (!originalCar) {
+      res.send({
+        error: "IdNotFoundError",
+        name: "IdNotFoundError",
+        message: `Car ${id} not found`,
+      });
+    }
+
+    if (user.isAdmin) {
+      const updatedCar = await updateCar(carId, updateFields);
+      res.send(updatedCar);
+    } else {
+      res.send({
+        error: "Unauthorized",
+        name: "User",
+        message: "Administrator required",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+
+
+//Extra Needed?
 
 // GET/cars/:id
 carsRouter.get("/:id", async (req, res, next) => {
@@ -83,57 +183,6 @@ carsRouter.get("/:color", requireUser, async (req, res, next) => {
   }
 });
 
-carsRouter.patch("/:carId", async (req, res, next) => {
-  const { carId } = req.params;
-  const updateFields = {};
-  const { manufacturer, model, type, color, price } = req.body;
 
-  if (manufacturer) {
-    updateFields.manufacturer = manufacturer;
-  }
-
-  if (model) {
-    updateFields.model = model;
-  }
-
-  if (type) {
-    updateFields.type = type;
-  }
-
-  if (color) {
-    updateFields.color = color;
-  }
-
-  if (price) {
-    updateFields.price = price;
-  }
-
-  try {
-    const originalCar = await getCarsById(carId);
-    if (!originalCar) {
-      res.send({
-        error: "IdNotFoundError",
-        name: "IdNotFoundError",
-        message: `Car ${id} not found`,
-      });
-    }
-
-    if (user.isAdmin) {
-      const updatedCar = await updateCar(carId, updateFields);
-      res.send(updatedCar);
-    } else {
-      res.send({
-        error: "Unauthorized",
-        name: "User",
-        message: "Administrator required",
-      });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-// POST /api/cars
-carsRouter.post("/", async (req, res, next) => {});
 
 module.exports = carsRouter;
--- db/db_adaptors/users.js
+++ db/db_adaptors/users.js
@@ -1,13 +1,18 @@
 const client = require('../client');
+const bcrypt = require('bcrypt');
 
 async function createUser({ username, password, isAdmin }) {
+  const SALT_COUNT = 10;
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+
   try {
     const { rows: [ user ] } = await client.query(/*sql*/`
       INSERT INTO users (username, password, "isAdmin") 
       VALUES ($1, $2, $3)
       ON CONFLICT (username) DO NOTHING
       RETURNING *;
-    `, [ username, password, isAdmin ]);
+    `, [ username, hashedPassword, isAdmin ]);
+    delete user.Password;
     return user;
   } catch (error) {
     console.error("Error creating user!", error);
@@ -30,10 +35,11 @@ async function getAllUsers() {
 
 async function getUser({ username, password }) {
   const user = await getUserByUsername(username);
-  if (user.password === password) {
-    return user; 
-  } else {
-    console.error("Error fetching user by ID!", error);
+  const hashedPassword = user.password;
+  const isValid = await bcrypt.compare(password, hashedPassword)
+  if (isValid) {
+    delete user.password;
+    return user;
   }
 }
 
@@ -44,6 +50,7 @@ async function getUserById(userId) {
       FROM users 
       WHERE id = $1;
     `, [ userId ]);
+    delete user.password
     return user;
   } catch (error) {
     console.error("Error fetching user by ID!", error);
