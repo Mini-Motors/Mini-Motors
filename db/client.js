9d74a69f0c42beb96e9be894eed00173199f56c4
Kevin Harp <kharp1984@gmail.com>
2023-07-03 13:29:45 -0400

    updated client.js to localhost

--- db/client.js
+++ db/client.js
@@ -6,7 +6,7 @@ const  { DATABASE_URL } = process.env;
 // change the DB_NAME string to whatever your group decides on
 const DB_NAME = "minimotors";
 
-const DB_URL = DATABASE_URL || `postgres://pi-ai:5432/${DB_NAME}`;
+const DB_URL = DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
 const client = new Client(DB_URL);
 
