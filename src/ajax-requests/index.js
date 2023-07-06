009f6a81271a13b1b656c0744fbe4d7e2c99c361
Kevin Harp <95554314+indi1984@users.noreply.github.com>
2023-07-05 20:08:13 -0400

    add users ajax requests
    Added users ajax requests for example

--- src/ajax-requests/index.js
+++ src/ajax-requests/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export const BASE_URL = 'localhost:4000/api';
+
 // this file holds your frontend network request adapters
 // think about each function as a service that provides data
 // to your React UI through AJAX calls
@@ -18,4 +20,101 @@ export async function getAPIHealth() {
 // AJAX Fetch REQUESTS HERE:
 
 
+// USERS
+export const registerUser = async (user) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        user,
+      )
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const registeredUser = async (user) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        user
+      )
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const myData = async (token) => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/me`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const result = await response.json();
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  export const myCarts = async (username, token) => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/${username}/allCarts`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const result = await response.json();
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  export const myActiveCarts = async (username, token) => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/${username}/activeCarts`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const result = await response.json();
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+// CARS
+export const allCars = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/cars`, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
