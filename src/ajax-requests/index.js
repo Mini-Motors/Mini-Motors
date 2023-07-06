d4a605b3cfebdb379c2c2f23e668ed586a70bee7
Kevin Harp <kharp1984@gmail.com>
2023-07-05 20:01:40 -0400

    updated axios to ajax requests

--- src/ajax-requests/index.js
+++ src/ajax-requests/index.js
@@ -4,20 +4,6 @@ import axios from 'axios';
 // think about each function as a service that provides data
 // to your React UI through AJAX calls
 
-// for example, if we need to display a list of users
-// we'd probably want to define a getUsers service like this:
-
-/* 
-  export async function getUsers() {
-    try {
-      const { data: users } = await axios.get('/api/users')
-      return users;
-    } catch(err) {
-      console.error(err)
-    }
-  }
-*/
-
 export async function getAPIHealth() {
   try {
     const { data } = await axios.get('/api/health');
@@ -27,3 +13,9 @@ export async function getAPIHealth() {
     return { healthy: false };
   }
 }
+
+
+// AJAX Fetch REQUESTS HERE:
+
+
+
