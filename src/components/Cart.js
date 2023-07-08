9ea697f4175dbcefef45091cffd12b6176dbde12
Kevin Harp <kharp1984@gmail.com>
2023-07-08 10:47:13 -0400

    added cart Comonent and got api health to load again

---
+++ src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+const Cart = () => {
+
+useEffect(() => {
+}, []);
+
+  return (
+    <h1>TEST FOR CART</h1>
+  )
+};
+
+export default Cart;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as App } from './App';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
+export { default as Cart } from './Cart';
 export { default as Home } from './Home';
