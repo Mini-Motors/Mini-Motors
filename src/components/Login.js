63c8449eb7949e5cff7b318f50c84342bac76d64
Kevin Harp <kharp1984@gmail.com>
2023-07-12 21:38:55 -0400

    use myActiveCarts to get cart id for cartDetails.  cartID is not working.

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { registeredUser, createCart, myActiveCarts } from "../ajax-requests/index.js";
 
 const Login = (props) => {
-  const { setToken, token, setCurrentUser, currentUser, setCartId, cartId } = props;
+  const { setToken, setCurrentUser, setCartId } = props;
   const [ username, setUsername ] = useState("");
   const [ password, setPassword ] = useState("");
    
@@ -13,24 +13,21 @@ const Login = (props) => {
       
     if (!results.error) {
       setToken(results.token);
-      console.log(results)
       window.localStorage.setItem("token",results.token);
       setCurrentUser(username);
       window.localStorage.setItem("currentUser",username);
-      const activeCarts = await myActiveCarts(currentUser, token)
+      const activeCarts = await myActiveCarts(username, results.token)
 
       if (!activeCarts) {
         const cart = {
             favorites: false,
           }
-          const setCart = await createCart(token, cart);
+          const setCart = await createCart(results.token, cart);
           setCartId(setCart.id);
       } else {
-        setCartId(setCart.id);
-        console.log(cartId)
+        setCartId(activeCarts.id);
       }
-      
-      // location.href = "/";
+       location.href = "/";
     } else {
       window.alert("Username and/or Password not accepted!")
     }
